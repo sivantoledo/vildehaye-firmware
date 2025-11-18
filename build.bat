94fe66e1776010506bf739d21fe0de2bf028b141
Sivan Toledo <stoledo@tau.ac.il>
2025-11-18 10:43:57 +0200

    no #ifdef CONFIG_H, it's used in config-*.h instead

--- build.bat
+++ build.bat
@@ -7,6 +7,16 @@ IF "%1"=="" (
   goto :usage
 )
 
+setlocal EnableDelayedExpansion
+if "%1"=="list" (
+    :: dir vildehaye-common\config-*.h
+    for /f "delims=" %%A in ('dir "vildehaye-common\config-*.h" /b /a-d 2^>nul') do (
+        set "filename=%%~nA"
+        echo !filename:config-=!
+    )
+    goto :EOF
+)
+
 set "CONFIG=%~1"
 
 echo CONFIG: %CONFIG%
@@ -14,7 +24,7 @@ echo CONFIG: %CONFIG%
 set HEADER_FILE="vildehaye-common\config-%CONFIG%.h"
 
 if not exist "%HEADER_FILE%" (
-    echo ERROR: File not founded: %HEADER_FILE%
+    echo ERROR: File not found: %HEADER_FILE%
     exit /b 1
 )
 
@@ -42,21 +52,21 @@ set "PROJECT=vildehaye-%ARCH%-%SC%"
 echo PROJECT: %PROJECT%
 
 if not exist "%PROJECT%" (
-    echo ERROR: Project not founded: %PROJECT%
+    echo ERROR: Project not found: %PROJECT%
     exit /b 1
 )
 
 cd "%PROJECT%"
 dir
 
-echo #ifndef CONFIG_H > config.h
-echo #define CONFIG_H >> config.h
+:: echo #ifndef CONFIG_H > config.h
+:: echo #define CONFIG_H >> config.h
 
-echo /* Automatically generated %DATE% %TIME% */ >> config.h
+echo /* Automatically generated %DATE% %TIME% */ > config.h
 
-echo #include ^"../vildehaye-common/config-tag-v2-6.h^" >> config.h
+echo #include ^"../vildehaye-common/config-%CONFIG%.h^" >> config.h
 
-echo #endif >> config.h
+:: secho #endif >> config.h
 
 cd Debug 
 
@@ -65,9 +75,14 @@ cd Debug
 
 dir *.hex
 
+ECHO copy %PROJECT%.hex ..\..\firmware\vildehaye-%CONFIG%.hex
+copy %PROJECT%.hex ..\..\firmware\vildehaye-%CONFIG%.hex
+
 goto :eof
 
 :usage
 ECHO build CONFIG
 ECHO For example, build tag-v2-6
+ECHO or,
+ECHO build list
 
